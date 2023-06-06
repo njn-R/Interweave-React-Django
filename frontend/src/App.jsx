9ef40275875c783412b266a87fd3c45b514410fc
njn-R <neerajnaveen364@gmail.com>
2023-06-06 05:32:35 +0530

    Started react homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import { Hero } from './components/Hero'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
